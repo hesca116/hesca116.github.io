db74b003213abf1ac2bb563baf252ed9e091771b
hesca116 <71509095+hesca116@users.noreply.github.com>
2020-10-29 08:40:56 -0600

    fixed windchill.js replaced .value with .innerHTML

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -1,6 +1,5 @@
-let temperature = parseFloat(document.getElementById('temp').value);
-alert(temperature);
-let windSpeed = parseFloat(document.getElementById('wSpeed').value);
+let temperature = parseFloat(document.getElementById('temp').innerHTML);
+let windSpeed = parseFloat(document.getElementById('wSpeed').innerHTML);
 let chill = windChill(temperature, windSpeed);
 
 //output 
@@ -8,6 +7,7 @@ document.getElementById('calculatedWChill').innerHTML = chill;
 
 function windChill(tempF, speed) {
   var chillFactor = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
-  return chillFactor;
+  var result = Math.round(chillFactor*100)/100
+  return result;
 }
 
