19043b9c75bf630aa6cf9e47eb04b3cd09afd8cb
hesca116 <71509095+hesca116@users.noreply.github.com>
2020-10-30 10:03:28 -0600

    trying to fix lazyloader

--- lesson07/js/lazyload.js
+++ lesson07/js/lazyload.js
@@ -7,13 +7,6 @@ const imgOptions = {
   rootMargin: "0px 0px 50px 0px"
 };
 
-//Arrow function with two instructions. 1. replace the data-src attribute with src, so it becomes the loaded image
-// , and 2. remove the  data-src attribute  
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onLoad = () => {image.removeAttribute('data-src');};
-}
-
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
@@ -28,6 +21,39 @@ if('IntersectionObserver' in window) {
   });
 } else {
   imagesToLoad.forEach((img) => {
+    alert("intersecting did not work");
     loadImages(img);
   });
 }
+
+//Arrow function with two instructions. 1. replace the data-src attribute with src, so it becomes the loaded image
+// , and 2. remove the  data-src attribute  
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onLoad = () => {image.removeAttribute('data-src');};
+}
+
+
+/*the other approach. Tried this and it didnt work.
+//this part checks if the IntersectionObserver supported? 
+if('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, observer) =>{
+    items.forEach((item) =>{
+      if(item.isIntersecting){
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+//forEach loop cicles through the images to load them when the imageObserver catches them
+imagesToLoad.forEach((img) => {
+  imgObserver.observe(img);
+  });
+}
+else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+*/
